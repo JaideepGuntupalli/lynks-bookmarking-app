94c27fd59d2da14c6be0b70cae3743ad3b46200a
Jaideep <guntupallijaideep@gmail.com>
2022-07-04 13:25:11 +0530

    Feat: Added TailwindCSS support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
+import '../styles/global.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const Home: FC = () => {
-  return <h1>Home</h1>;
+  return <h1 className="text-xl">Home</h1>;
 };
 
 export default Home;
