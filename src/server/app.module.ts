9ec21fe2fb1dd12845c4484e763decf4ed1bff8c
Jaideep <guntupallijaideep@gmail.com>
2022-07-04 12:29:30 +0530

    Feat: Added acquaintance between the frameworks

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectModule } from './project/project.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
+    RenderModule.forRootAsync(
+      Next({ dev: true }) /* null means that nest-next 
+    should look for pages in root dir */,
+      { viewsDir: null },
+    ),
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UserModule,
@@ -15,5 +23,6 @@ import { ProjectModule } from './project/project.module';
     PrismaModule,
     ProjectModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
