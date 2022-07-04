3d5f7eea995fc97a3cc012762fc91e4ea077e480
Jaideep <guntupallijaideep@gmail.com>
2022-07-04 16:34:59 +0530

    Feat: Added Poppins and DM Sans fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      dm: ["DM Sans", ...defaultTheme.fontFamily.serif],
+      poppins: ["Poppins", ...defaultTheme.fontFamily.serif],
+  },
     extend: {},
   },
   plugins: [],
